220994be306bbfba33db852e01cdf924b1d61e82
Andrei Bucur <abucur980@gmail.com>
2021-11-04 10:12:56 +0200

    came back to initial commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-alert('hello');
+const input = document.querySelector('#input');
+const btn = document.querySelector('#btn');
+const uList = document.getElementsByTagName('ul')[0];
+
+
+btn.addEventListener('click', addElementOnClick);
+input.addEventListener('keypress', addElementOnKey);
+
+function createListElement(){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    uList.appendChild(li);  
+    input.value="";
+
+// remove the item
+    li.addEventListener('click', function (){
+        li.classList.toggle('done');
+    })
+}
+
+function addElementOnClick (){
+    if(input.value.length>0){
+        createListElement();
+    }
+}
+
+function addElementOnKey(event){
+    if(input.value.length>0 && event.keyCode === 13){
+        createListElement();
+    }
+}
+
+
